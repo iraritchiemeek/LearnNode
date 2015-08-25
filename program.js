88f9485f8f82c0f49a1e42aec01db5b6407f5361
iraritchiemeek <iraritchiemeek@gmail.com>
2015-08-25 13:40:33 +1200

    Complete character counting challenge

--- program.js
+++ program.js
@@ -1,8 +1,16 @@
 var http = require('http')
+var full_res = ''
 
 http.get(process.argv[2], function callback(response) {
+	
 	response.setEncoding('utf8')
+	
 	response.on('data', function(data) {
-		console.log(data)
+		full_res += data
+	})
+
+	response.on('end', function(){
+		console.log(full_res.length)
+		console.log(full_res)
 	})
 })
